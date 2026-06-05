c5c311e12870b5fb6c4480f14237dc7c1fd623eb
Abhiuday <gamingpro8953@gmail.com>
2026-06-05 15:08:04 +0530

    Added Gemini & AI Routes and SUM Functions

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
+const { GoogleGenAI } = require("@google/genai");
+
 
 const { App } = require("@slack/bolt");
 const axios = require("axios");
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_TOKEN });
+
 
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
@@ -23,7 +27,11 @@ app.command("/hbsc-help", async ({ ack, respond }) => {
             `Available Commands:
 /hbsc-help - List available commands
 /hbsc-ping - Check bot latency
-/hbsc-joke - Get a Joke`
+/hbsc-joke - Get a Joke
+/hbsc-sum [num1] [num2] - Get the sum of two numbers
+/hbsc-gemini - [question?] - Ask Gemini a question
+`
+
     });
 });
 
@@ -32,15 +40,53 @@ app.command("/hbsc-joke", async ({ ack, respond }) => {
     console.log("Fetching a joke...");
     try {
         const response = await axios.get("https://official-joke-api.appspot.com/jokes/random");
-        await respond({ text: `Joke:\n${response.data.setup},  ${response.data.punchline}` });
-        console.log(`joke:${response.data.setup}`)
+        await respond({ text: `Joke: \n${response.data.setup}, ${response.data.punchline}` });
+        console.log(`joke: ${response.data.setup}`)
     } catch (err) {
         await respond({ text: "Failed to fetch a Joke (Partner Server Issue)." });
         console.log(err);
     }
 });
 
+app.command("/hbsc-sum", async ({ command, ack, respond }) => {
+    await ack();
 
+    const args = command.text.split(" ");
+
+    const a = Number(args[0]);
+    const b = Number(args[1]);
+
+    if (isNaN(a) || isNaN(b)) {
+        return respond("I think you give me an invalid number please check your input and try again :) ❤️");
+    }
+
+    await respond(`Result: ${a + b}`);
+});
+
+app.command("/hbsc-gemini", async ({ command, ack, respond, client }) => {
+    await ack();
+    console.log("Generating response from Gemini...");
+    try {
+        const question = command.text.trim();
+        if (!question) {
+            await respond(`I think you just call me! without any question :)`);
+        }
+        await respond("thinking....")
+
+        await respond("Genrating Answer , Please wait")
+
+        const response = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: question,
+        });
+
+        console.log(response);
+
+        await respond(response.text)
+    } catch (err) {
+        respond("Ai Model is Overloaded try again!")
+    }
+});
 (async () => {
     await app.start();
     console.log("bot is running!");
